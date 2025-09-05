728ccd164bf07034fc1b6c3aa4d155686e91b234
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-05 12:34:50 +0800

    Refactor user types to separate Users and UserDetails for improved clarity

--- src/@types/models/user.ts
+++ src/@types/models/user.ts
@@ -1,10 +1,20 @@
-export type UserType = {
+import mongoose from "mongoose";
+
+export type Users = {
   username: string;
   password: string;
-  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type UserDetails = {
+  user: mongoose.Types.ObjectId | Users;
   fullname: string;
+  email: string;
   contact_number: string;
   address: string;
   createdAt: Date;
   updatedAt: Date;
 };
+
+export type UserType = Users & UserDetails;
