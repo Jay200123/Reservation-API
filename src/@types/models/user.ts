b807edc0cfdeb4d6253cf3806e9b522516758b1b
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-05 11:49:50 +0800

    Add UserType definition for user model

---
+++ src/@types/models/user.ts
@@ -0,0 +1,10 @@
+export type UserType = {
+  username: string;
+  password: string;
+  email: string;
+  fullname: string;
+  contact_number: string;
+  address: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
