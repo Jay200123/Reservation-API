1f8a5ec403da5caebd8038f7697f30037d5604ea
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-05 20:48:03 +0800

    Refactor UserController to improve method structure and logging consistency

--- src/@routes/users/controller.ts
+++ src/@routes/users/controller.ts
@@ -1,5 +1,64 @@
 import UserService from "./service";
+import { MiddlewareFn } from "../../@types";
+import { SuccessHandler, logger } from "../../@utils";
+import { STATUSCODE } from "../../@constants";
 
 export default class UserController {
-  constructor(userService: UserService) {}
+  constructor(private userService: UserService) {}
+
+  getAllUsers: MiddlewareFn = async (req, res) => {
+    logger.info({
+      GET_ALL_USERS_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.userService.getAllUsers();
+
+    logger.info({
+      GET_ALL_USERS_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(res, STATUSCODE.SUCCESS, result, "Success");
+  };
+
+  getUserById: MiddlewareFn = async (req, res) => {
+    logger.info({
+      GET_USER_BY_ID_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.userService.getUserById(req.params.user_id);
+
+    logger.info({
+      GET_USER_BY_ID_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(res, STATUSCODE.SUCCESS, result, "Success");
+  };
+
+  createUser: MiddlewareFn = async (req, res) => {
+    logger.info({
+      CREATE_USER_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.userService.createUser({
+      ...req.body,
+    });
+
+    logger.info({
+      CREATE_USER_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(res, STATUSCODE.CREATED, result, "Success");
+  };
 }
