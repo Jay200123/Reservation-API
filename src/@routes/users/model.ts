1ab1349a86468338c5c20cf8c1b20122b1ba6a5d
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-05 12:34:43 +0800

    Refactor UserSchema to use Users type and enhance schema definition

--- src/@routes/users/model.ts
+++ src/@routes/users/model.ts
@@ -1,8 +1,32 @@
 import { Schema, model } from "mongoose";
-import { UserType } from "../../@types";
+import { Users } from "../../@types";
+import { RESOURCE } from "../../@constants";
 
-export const UserSchema = new Schema<UserType>({});
+/**
+ * User Schema
+ * @author Jay
+ * @description Mongoose Schema for Users
+ */
+export const UserSchema = new Schema<Users>({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const User = model<UserType>("users", UserSchema);
+// Here we are creating a Mongoose model named 'User' using the UserSchema and associating it with the 'users' collection in the database.
+const User = model<Users>(RESOURCE.USERS, UserSchema);
 
 export default User;
