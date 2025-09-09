5cd75ead8fa8b50460b2832f36d80ba054ecb125
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-09 22:07:53 +0800

    Added comment on hashPassword async module

--- src/@utils/hashPassword.ts
+++ src/@utils/hashPassword.ts
@@ -1,5 +1,11 @@
 import { hash } from "bcrypt";
 
+/**
+ * Hashes a plain-text password using bcrypt.
+ *
+ * @param password - The plain-text password to hash.
+ * @returns A promise that resolves to the hashed password string.
+ */
 export const hashPassword = async (password: string): Promise<string> => {
   return await hash(password, Number(process.env.SALT_ROUNDS));
 };
