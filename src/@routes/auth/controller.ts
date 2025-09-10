8f1335ee869de0ff4ae9c5c209cdfd294ddee41b
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-10 13:45:48 +0800

    Add new registerUser async Request Response layer for register user request.

--- src/@routes/auth/controller.ts
+++ src/@routes/auth/controller.ts
@@ -1,5 +1,32 @@
-import { AuthService } from "./service";
+import { MiddlewareFn } from "../../@types";
+import AuthService from "./service";
+import { logger, SuccessHandler, valdiateFields } from "../../@utils";
+import { STATUSCODE } from "../../@constants";
+export default class AuthController {
+  constructor(private authService: AuthService) {}
 
-export class AuthController {
-  constructor(private authRepository: AuthService) {}
+  registerUser: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      REGISTER_USER_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+    // Validate request fields
+    valdiateFields(req);
+
+    const result = await this.authService.registerUser(req.body);
+
+    logger.info({
+      REGISTER_USER_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(
+      res,
+      STATUSCODE.CREATED,
+      result,
+      "User registered successfully"
+    );
+  };
 }
