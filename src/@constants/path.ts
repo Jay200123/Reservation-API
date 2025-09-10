8233a894580e90023123bde2404bff5be1ed6745
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-10 10:46:04 +0800

    ADD REFRESH path constant

--- src/@constants/path.ts
+++ src/@constants/path.ts
@@ -2,6 +2,7 @@ export const PATH = {
   API: "/api/v1",
   REGISTER: "/register",
   LOGIN: "/login",
+  REFRESH: "/refresh",
   LOGOUT: "/logout",
   GET_ALL_USERS: "/users/all",
   GET_USER_BY_USER_ID: "/users/:user_id",
