fd646f99e098a089b0363f70bea64b76766eac54
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-10 10:48:58 +0800

    Add AuthRepository class with mongoose query methods for  retrieving credentials & insert credential into database.

---
+++ src/@routes/auth/repository.ts
@@ -0,0 +1,18 @@
+import { Model } from "mongoose";
+import { UserCredentials } from "../../@types";
+
+export class AuthRepository {
+  constructor(private userCredentialModel: Model<UserCredentials>) {}
+
+  async getOneByAccessToken(access_token: string) {
+    return await this.userCredentialModel.findOne({ access_token });
+  }
+
+  async getOneByRefreshToken(refresh_token: string) {
+    return await this.userCredentialModel.findOne({ refresh_token });
+  }
+
+  async addCredential(data: Omit<UserCredentials, "createdAt" | "updatedAt">) {
+    return await this.userCredentialModel.create({ ...data });
+  }
+}
