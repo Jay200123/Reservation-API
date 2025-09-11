0eb742f824bd3bf738ba402d35fc49d06cae9c51
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-11 20:23:28 +0800

    Export service model.

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,4 +1,5 @@
 export * from "./models/user";
 export * from "./models/settings";
 export * from "./models/user_credentials";
+export * from "./models/service";
 export * from "./middleware/index";
