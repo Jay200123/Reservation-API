f1eaf281cd9defbff02f91c19a144e70401e153d
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-11 20:57:21 +0800

    Changed ServiceServices to handle service CRUD related business logic

--- src/@routes/service/service.ts
+++ src/@routes/service/service.ts
@@ -1,15 +1,65 @@
 import ServiceRepository from "./repository";
-
+import { ErrorHandler, logger } from "../../@utils";
+import { STATUSCODE } from "../../@constants";
+import mongoose from "mongoose";
+import { Service } from "../../@types";
 export default class ServiceServices {
   constructor(private serviceRepository: ServiceRepository) {}
 
-  async getAllServices() {}
+  async getAllServices() {
+    const result = await this.serviceRepository.getAll();
 
-  async getServiceById() {}
+    if (!result) {
+      throw new ErrorHandler(STATUSCODE.NOT_FOUND, "Services not found");
+    }
 
-  async createService() {}
+    return result;
+  }
 
-  async updateServiceById() {}
+  async getServiceById(service_id: string) {
+    if (service_id == ":service_id") {
+      throw new ErrorHandler(STATUSCODE.BAD_REQUEST, "Missing service_id");
+    }
 
-  async deleteServiceById() {}
+    if (!mongoose.Types.ObjectId.isValid(service_id)) {
+      throw new ErrorHandler(STATUSCODE.BAD_REQUEST, "Invalid Request");
+    }
+
+    const result = await this.serviceRepository.getById(service_id);
+
+    if (!result) {
+      throw new ErrorHandler(STATUSCODE.NOT_FOUND, "Service not found");
+    }
+
+    return result;
+  }
+
+  async createService(data: Omit<Service, "createdAt" | "updatedAt">) {
+    const result = await this.serviceRepository.create(data);
+
+    return result;
+  }
+
+  async updateServiceById(service_id: string, data: Partial<Service>) {
+    const service = await this.serviceRepository.getById(service_id);
+
+    if (!service) {
+      logger.info({
+        UPDATE_SERVICE_ERROR: {
+          message: "Service not found",
+        },
+      });
+      throw new ErrorHandler(STATUSCODE.BAD_REQUEST, "Invalid Request");
+    }
+
+    const result = await this.serviceRepository.updateById(service_id, data);
+
+    return result;
+  }
+
+  async deleteServiceById(service_id: string) {
+    const result = await this.serviceRepository.deleteById(service_id);
+
+    return result;
+  }
 }
