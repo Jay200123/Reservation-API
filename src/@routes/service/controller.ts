1dca17b8c54f43fea5f4b2a282d4b4970f7d4989
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-11 20:56:16 +0800

    Changed Service Controller to handle all service data Request.

--- src/@routes/service/controller.ts
+++ src/@routes/service/controller.ts
@@ -1,5 +1,115 @@
 import ServiceServices from "./service";
+import { MiddlewareFn } from "../../@types";
+import { SuccessHandler, logger } from "../../@utils";
+import { STATUSCODE } from "../../@constants";
 
 export default class ServiceController {
-  constructor(private serviceController: ServiceServices) {}
+  constructor(private serviceServices: ServiceServices) {}
+
+  getAllServices: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      GET_ALL_SERVICES_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.serviceServices.getAllServices();
+
+    logger.info({
+      GET_ALL_SERVICES_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(res, STATUSCODE.SUCCESS, result, "Success");
+  };
+
+  getServiceById: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      GET_SERVICE_BY_ID_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.serviceServices.getServiceById(
+      req.params.service_id
+    );
+
+    logger.info({
+      GET_SERVICE_BY_ID_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(res, STATUSCODE.SUCCESS, result, "Success");
+  };
+
+  createService: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      CREATE_SERVICE_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.serviceServices.createService(req.body);
+
+    logger.info({
+      CREATE_SERVICE_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(
+      res,
+      STATUSCODE.CREATED,
+      result,
+      "New service created successfully."
+    );
+  };
+
+  updateServiceById: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      UPDATE_SERVICE_BY_ID_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.serviceServices.updateServiceById(
+      req.params.service_id,
+      req.body
+    );
+
+    logger.info({
+      UPDATE_SERVICE_BY_ID_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(
+      res,
+      STATUSCODE.SUCCESS,
+      result,
+      "Service updated successfully."
+    );
+  };
+
+  deleteServiceById: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      DELETE_SERVICE_BY_ID_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.serviceServices.deleteServiceById(
+      req.params.service_id
+    );
+
+    logger.info({
+      DELETE_SERVICE_BY_ID_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(res, STATUSCODE.SUCCESS, result, "Success");
+  };
 }
