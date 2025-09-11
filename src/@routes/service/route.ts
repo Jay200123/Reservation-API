6afc3078e24886363b3692c8a856a520e59dd890
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-11 20:57:41 +0800

    Add new route endpoints for each service CRUD operations.

--- src/@routes/service/route.ts
+++ src/@routes/service/route.ts
@@ -1,5 +1,36 @@
 import express from "express";
+import Service from "./model";
+import ServiceRepository from "./repository";
+import ServiceServices from "./service";
+import ServiceController from "./controller";
+import { createServiceValidation } from "../../@validations";
+import { PATH } from "../../@constants";
 
+//ADD access token middleware.
 const router = express.Router();
 
+const serviceRepository = new ServiceRepository(Service);
+
+const serviceServices = new ServiceServices(serviceRepository);
+
+const serviceController = new ServiceController(serviceServices);
+
+//get all users endpoint.
+router.get(PATH.GET_ALL_SERVICES, serviceController.getAllServices);
+
+//get one user endpoint.
+router.get(PATH.GET_SERVICE_BY_ID, serviceController.getServiceById);
+
+//create service endpoint.
+router.post(
+  PATH.CREATE_SERVICE,
+  createServiceValidation,
+  serviceController.createService
+);
+
+//update service endpoint.
+router.patch(PATH.EDIT_SERVICE_BY_ID, serviceController.updateServiceById);
+
+//delete service endpoint.
+router.delete(PATH.DELETE_SERVICE_BY_ID, serviceController.deleteServiceById);
 export default router;
