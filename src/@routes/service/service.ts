f603eb07ef8b09d8533a338f818382737fc57295
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-11 20:21:39 +0800

    Add UserService class to handle service-related business logic.

---
+++ src/@routes/service/service.ts
@@ -0,0 +1,15 @@
+import ServiceRepository from "./repository";
+
+export default class ServiceServices {
+  constructor(private serviceRepository: ServiceRepository) {}
+
+  async getAllServices() {}
+
+  async getServiceById() {}
+
+  async createService() {}
+
+  async updateServiceById() {}
+
+  async deleteServiceById() {}
+}
