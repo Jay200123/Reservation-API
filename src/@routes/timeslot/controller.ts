a95d42d7712b136c763090ca504d050d300685c0
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-14 21:42:15 +0800

    Add new TimeslotController class for handling Request Response timeslot layer.

---
+++ src/@routes/timeslot/controller.ts
@@ -0,0 +1,116 @@
+import TimeslotService from "./service";
+import { MiddlewareFn } from "../../@types";
+import { logger, SuccessHandler } from "../../@utils";
+import { STATUSCODE } from "../../@constants";
+
+export default class TimeslotController {
+  constructor(private timeslotService: TimeslotService) {}
+
+  getAllTimeslots: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      GET_ALL_TIMESLOTS_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.timeslotService.getAllTimeslots();
+
+    logger.info({
+      GET_ALL_TIMESLOTS_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(res, STATUSCODE.SUCCESS, result, "Success");
+  };
+
+  getAllTimeslotById: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      GET_ALL_TIMESLOTS_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.timeslotService.getTimeslotById(req.params.id);
+
+    logger.info({
+      GET_ALL_TIMESLOTS_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(res, STATUSCODE.SUCCESS, result, "Success");
+  };
+
+  createTimeslot: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      GET_ALL_TIMESLOTS_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.timeslotService.createTimeslot(req.body);
+
+    logger.info({
+      GET_ALL_TIMESLOTS_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(
+      res,
+      STATUSCODE.CREATED,
+      result,
+      "Timeslot created successfully."
+    );
+  };
+
+  updateTimeslotById: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      GET_ALL_TIMESLOTS_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.timeslotService.updateTimeslotById(
+      req.params.id,
+      req.body
+    );
+
+    logger.info({
+      GET_ALL_TIMESLOTS_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(
+      res,
+      STATUSCODE.SUCCESS,
+      result,
+      "Timeslot updated successfully."
+    );
+  };
+
+  deleteTimeslotById: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      GET_ALL_TIMESLOTS_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.timeslotService.deleteTimeslotById(req.params.id);
+
+    logger.info({
+      GET_ALL_TIMESLOTS_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(
+      res,
+      STATUSCODE.SUCCESS,
+      result,
+      "Timeslot deleted successfully."
+    );
+  };
+}
