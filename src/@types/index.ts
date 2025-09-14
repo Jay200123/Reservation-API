d6b0f063dabd29e41355850947c94aa6fd15f313
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-14 21:45:02 +0800

    Export timeslots type.

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -2,4 +2,5 @@ export * from "./models/user";
 export * from "./models/settings";
 export * from "./models/user_credentials";
 export * from "./models/service";
+export * from "./models/timeslots";
 export * from "./middleware/index";
