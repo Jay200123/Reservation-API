27f865902e8fba766b98150d22ba777a829d4b09
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-14 21:42:45 +0800

    Add new TimeslotRepository class for handling data-management related to timeslots.

---
+++ src/@routes/timeslot/repository.ts
@@ -0,0 +1,30 @@
+import { Timeslot } from "../../@types";
+import { Model } from "mongoose";
+
+export default class TimeslotRepository {
+  constructor(private timeslotModel: Model<Timeslot>) {}
+
+  async getAll() {
+    return await this.timeslotModel.find().exec();
+  }
+
+  async getById(id: string) {
+    return await this.timeslotModel.findById(id);
+  }
+
+  async create(data: Omit<Timeslot, "createdAt" | "updatedAt">) {
+    return await this.timeslotModel.create({ ...data });
+  }
+
+  async updateById(id: string, data: Partial<Timeslot>) {
+    return await this.timeslotModel.findByIdAndUpdate(
+      id,
+      { ...data, updatedAt: Date.now },
+      { new: true }
+    );
+  }
+
+  async deleteById(id: string) {
+    return await this.timeslotModel.findByIdAndDelete(id);
+  }
+}
