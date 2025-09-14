30f678480943212d28a814775af94fcac87073ad
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-14 21:41:53 +0800

    Add authMiddleware for Verifying authenticated users before accessing service endpoint routes.

--- src/@routes/service/route.ts
+++ src/@routes/service/route.ts
@@ -1,36 +1,67 @@
 import express from "express";
 import Service from "./model";
+import Settings from "../settings/model";
+import UserCredentials from "../auth/model";
+import SettingsRepository from "../settings/repository";
+import AuthRepository from "../auth/repository";
 import ServiceRepository from "./repository";
 import ServiceServices from "./service";
 import ServiceController from "./controller";
 import { createServiceValidation } from "../../@validations";
+import { JWT } from "../../@utils";
 import { PATH } from "../../@constants";
+import { AuthMiddleware } from "../../@middleware";
 
 //ADD access token middleware.
 const router = express.Router();
 
+const settingsRepository = new SettingsRepository(Settings);
+const authRepository = new AuthRepository(UserCredentials);
 const serviceRepository = new ServiceRepository(Service);
 
 const serviceServices = new ServiceServices(serviceRepository);
-
 const serviceController = new ServiceController(serviceServices);
 
+const authMiddleware = new AuthMiddleware(
+  settingsRepository,
+  authRepository,
+  new JWT()
+);
+
 //get all users endpoint.
-router.get(PATH.GET_ALL_SERVICES, serviceController.getAllServices);
+router.get(
+  PATH.SERVICES,
+  authMiddleware.AccessTokenVerifier(),
+  serviceController.getAllServices
+);
 
 //get one user endpoint.
-router.get(PATH.GET_SERVICE_BY_ID, serviceController.getServiceById);
+router.get(
+  PATH.SERVICE_ID,
+  authMiddleware.AccessTokenVerifier(),
+  serviceController.getServiceById
+);
 
 //create service endpoint.
 router.post(
-  PATH.CREATE_SERVICE,
+  PATH.SERVICES,
+  authMiddleware.AccessTokenVerifier(),
   createServiceValidation,
   serviceController.createService
 );
 
 //update service endpoint.
-router.patch(PATH.EDIT_SERVICE_BY_ID, serviceController.updateServiceById);
+router.patch(
+  PATH.EDIT_SERVICE_ID,
+  authMiddleware.AccessTokenVerifier(),
+  serviceController.updateServiceById
+);
 
 //delete service endpoint.
-router.delete(PATH.DELETE_SERVICE_BY_ID, serviceController.deleteServiceById);
+router.delete(
+  PATH.SERVICE_ID,
+  authMiddleware.AccessTokenVerifier(),
+  serviceController.deleteServiceById
+);
+
 export default router;
