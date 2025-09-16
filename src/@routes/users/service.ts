ea4b7d9c90b0dfe23cff6cf2279687dfe3911928
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-16 20:28:40 +0800

    Changed UserService class by fixing & adding logs on getAllUsers, getUserById & adding validateFields on updateUser method.

--- src/@routes/users/service.ts
+++ src/@routes/users/service.ts
@@ -3,12 +3,17 @@ import UserDetailsRepository from "../user_details/repository";
 import { UserType } from "../../@types";
 import {
   ErrorHandler,
+  logger,
+  updateUserFields,
+  verifyFields,
 } from "../../@utils";
 import mongoose from "mongoose";
+import { STATUSCODE } from "../../@constants";
 
 export default class UserService {
   constructor(
     private userRepository: UserRepository,
+    private userDetailsRepository: UserDetailsRepository
   ) {}
 
   /**
@@ -16,7 +21,7 @@ export default class UserService {
    * @returns result
    */
   async getAllUsers() {
-    const result = await this.userRepository.getAll();
+    const result = await this.userDetailsRepository.getAllUserDetails();
 
     // Check if users exist
     if (!result.length) {
@@ -30,22 +35,36 @@ export default class UserService {
    * Retrieve specific user using user_id
    * @returns result
    */
-  async getUserById(user_id: string) {
-    // Check if user_id is provided
-    if (user_id == ":user_id") {
+  async getUserById(id: string) {
+    // Check if id is provided
+    if (id == ":id") {
       throw new ErrorHandler(400, "Missing user ID");
     }
 
-    // Validate user_id format
-    if (!mongoose.Types.ObjectId.isValid(user_id)) {
+    // Validate id format
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      logger.info({
+        GET_USER_BY_ID_REQUEST: {
+          message: "Invalid mongoose ID.",
+        },
+      });
+
       throw new ErrorHandler(400, "Invalid user ID");
     }
 
-    const result = await this.userRepository.getById(user_id);
+    const user = await this.userRepository.getById(id);
+
+    if (!user) {
+      throw new ErrorHandler(404, "User not found");
+    }
+
+    const result = await this.userDetailsRepository.getDetailsByUserId(id);
+
+    console.log({ result });
 
     // Check if user exists
     if (!result) {
-      throw new ErrorHandler(404, "User not found");
+      throw new ErrorHandler(404, "Details not found");
     }
 
     return result;
@@ -58,15 +77,32 @@ export default class UserService {
    * @returns result
    */
   async updateUser(id: string, data: Partial<UserType>) {
-    const user = await this.userRepository.getById(id);
+    console.log({ id });
 
-    if (id == ":user_id") {
+    if (id == ":id") {
       throw new ErrorHandler(400, "Missing user ID");
     }
+
+    const user = await this.userRepository.getById(id);
 
     if (!user) {
       throw new ErrorHandler(404, "User not found");
     }
+
+    //Checks if the req.body is empty or null.
+    if (!data || null) {
+      throw new ErrorHandler(
+        STATUSCODE.BAD_REQUEST,
+        `Missing required fields: ${updateUserFields.join(", ")}`
+      );
+    }
+
+    /**
+     * Verifies that all required fields exist in the given data object, if an unknown field exists it will throw an Error
+     * @param fields - An array of required field names to check (e.g. createUserFields).
+     * @param data - The object to validate, typically req.body.
+     */
+    verifyFields(updateUserFields, data);
 
     const result = await this.userRepository.updateById(id, data);
 
