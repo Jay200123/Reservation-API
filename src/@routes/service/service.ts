122b0c0ebd665a14cb122f9ccdb478a1cedf9fc2
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-17 16:46:34 +0800

    Changed getServiceById, createService & updateServiceById by fixing typo, adding & validationFields.

--- src/@routes/service/service.ts
+++ src/@routes/service/service.ts
@@ -1,5 +1,11 @@
 import ServiceRepository from "./repository";
-import { ErrorHandler, logger } from "../../@utils";
+import {
+  ErrorHandler,
+  logger,
+  verifyFields,
+  createServiceFields,
+  createUserFields,
+} from "../../@utils";
 import { STATUSCODE } from "../../@constants";
 import mongoose from "mongoose";
 import { Service } from "../../@types";
@@ -9,23 +15,29 @@ export default class ServiceServices {
   async getAllServices() {
     const result = await this.serviceRepository.getAll();
 
-    if (!result) {
+    if (!result.length) {
       throw new ErrorHandler(STATUSCODE.NOT_FOUND, "Services not found");
     }
 
     return result;
   }
 
-  async getServiceById(service_id: string) {
-    if (service_id == ":service_id") {
-      throw new ErrorHandler(STATUSCODE.BAD_REQUEST, "Missing service_id");
+  async getServiceById(id: string) {
+    if (id == ":id") {
+      throw new ErrorHandler(STATUSCODE.BAD_REQUEST, "Missing Service ID");
     }
 
-    if (!mongoose.Types.ObjectId.isValid(service_id)) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      logger.info({
+        GET_SERVICE_BY_ID_ERROR: {
+          message: "Invalid mongoose ID",
+        },
+      });
+
       throw new ErrorHandler(STATUSCODE.BAD_REQUEST, "Invalid Request");
     }
 
-    const result = await this.serviceRepository.getById(service_id);
+    const result = await this.serviceRepository.getById(id);
 
     if (!result) {
       throw new ErrorHandler(STATUSCODE.NOT_FOUND, "Service not found");
@@ -35,13 +47,20 @@ export default class ServiceServices {
   }
 
   async createService(data: Omit<Service, "createdAt" | "updatedAt">) {
+    /**
+     * Verifies that all required fields exist in the given data object, if an unknown field exists it will throw an Error
+     * @param fields - An array of required field names to check (e.g. createUserFields).
+     * @param data - The object to validate, typically req.body.
+     */
+    verifyFields(createServiceFields, data);
+
     const result = await this.serviceRepository.create(data);
 
     return result;
   }
 
-  async updateServiceById(service_id: string, data: Partial<Service>) {
-    const service = await this.serviceRepository.getById(service_id);
+  async updateServiceById(id: string, data: Partial<Service>) {
+    const service = await this.serviceRepository.getById(id);
 
     if (!service) {
       logger.info({
@@ -52,7 +71,22 @@ export default class ServiceServices {
       throw new ErrorHandler(STATUSCODE.BAD_REQUEST, "Invalid Request");
     }
 
-    const result = await this.serviceRepository.updateById(service_id, data);
+    //Checks if the data sent from the req body is empty or null.
+    if (!data || null) {
+      throw new ErrorHandler(
+        STATUSCODE.BAD_REQUEST,
+        `Missing required fields: ${createServiceFields.join(", ")}`
+      );
+    }
+
+    /**
+     * Verifies that all required fields exist in the given data object, if an unknown field exists it will throw an Error
+     * @param fields - An array of required field names to check (e.g. createUserFields).
+     * @param data - The object to validate, typically req.body.
+     */
+    verifyFields(createServiceFields, data);
+
+    const result = await this.serviceRepository.updateById(id, data);
 
     return result;
   }
