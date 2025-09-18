e537ecad466db5a0c63660dc4955179e2454eff5
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-18 20:15:30 +0800

    Add ReservationController class for Request Response layer for reservations API

---
+++ src/@routes/reservation/controller.ts
@@ -0,0 +1,105 @@
+import { MiddlewareFn } from "../../@types";
+import ReservationService from "./service";
+import { SuccessHandler, logger, valdiateFields } from "../../@utils";
+import { STATUSCODE } from "../../@constants";
+
+export default class ReservationController {
+  constructor(private reservationService: ReservationService) {}
+
+  getAllReservations: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      GET_RESERVATIONS_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.reservationService.getAllReservations();
+
+    logger.info({
+      GET_RESERVATIONS_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(
+      res,
+      STATUSCODE.SUCCESS,
+      result,
+      "Reservations retrieved successfully"
+    );
+  };
+
+  getReservationById: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      GET_RESERVATION_BY_ID_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.reservationService.getReservationById(
+      req.params.id
+    );
+
+    logger.info({
+      GET_RESERVATION_BY_ID_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(
+      res,
+      STATUSCODE.SUCCESS,
+      result,
+      "Reservation retrieved successfully"
+    );
+  };
+
+  createReservation: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      CREATE_RESERVATION_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.reservationService.createReservation(req.body);
+
+    logger.info({
+      CREATE_RESERVATION_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(
+      res,
+      STATUSCODE.SUCCESS,
+      result,
+      "Reservations created successfully"
+    );
+  };
+
+  updateReservationById: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      UPDATE_RESERVATION_BY_ID_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.reservationService.updateReservationById(
+      req.params.id,
+      req.body
+    );
+
+    logger.info({
+      UPDATE_RESERVATION_BY_ID_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(
+      res,
+      STATUSCODE.SUCCESS,
+      result,
+      "Reservation updated successfully"
+    );
+  };
+}
