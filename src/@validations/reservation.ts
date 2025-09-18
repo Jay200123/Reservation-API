3443a36a651223ef025eecb90e2dc5076e973ec7
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-18 20:19:08 +0800

    Add new reservation express validation array fields

---
+++ src/@validations/reservation.ts
@@ -0,0 +1,15 @@
+import { body } from "express-validator";
+
+export const createReservationFields = [
+  body("user").trim().escape().notEmpty().withMessage("user required"),
+  body("services").trim().escape().notEmpty().withMessage("services required"),
+  body("timeslot").trim().escape().notEmpty().withMessage("timeslot required"),
+  body("payment_type")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("payment_type required")
+    .bail()
+    .isIn(["CASH", "ONLINE_PAYMENT"])
+    .withMessage("payment_type must be 'CASH' OR 'ONLINE_PAYMENT' "),
+];
