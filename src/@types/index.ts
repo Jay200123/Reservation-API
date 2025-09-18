a7f2043c6ec471c0cb1694c60e8431bfcfe109ce
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-18 20:17:56 +0800

    Export  reservations model type

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -3,4 +3,5 @@ export * from "./models/settings";
 export * from "./models/user_credentials";
 export * from "./models/service";
 export * from "./models/timeslots";
+export * from "./models/reservations";
 export * from "./middleware/index";
