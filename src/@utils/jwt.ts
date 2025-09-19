11b9c07e9638b76c11992d267304a8366c0396ef
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-19 20:36:51 +0800

    Changed verifyAccessToken method from JWT class in using try catch block instead of callback for proper & simple access token verifying error checking.

--- src/@utils/jwt.ts
+++ src/@utils/jwt.ts
@@ -2,36 +2,33 @@ import jwt from "jsonwebtoken";
 import { ErrorHandler } from "./handlers";
 import { STATUSCODE } from "../@constants";
 import { logger } from "./logger";
+import { JWTPayload } from "../@types";
 
 //Need payload types for each methods
 // Check if all methods has pre defined return types. - meaning does each method requires to assign types on what they return ?
 
 export class JWT {
-  generateAccessToken(payload: any) {
+  generateAccessToken(payload: JWTPayload) {
     return jwt.sign(payload, process.env.JWT_ACCESS_SECRET_KEY as string, {
       expiresIn: "1h",
     });
   }
 
-  verifyAccessToken(access_token: any) {
-    return jwt.verify(
-      access_token,
-      process.env.JWT_ACCESS_SECRET_KEY as string,
-      (err: jwt.VerifyErrors | null, decoded: any) => {
-        if (err) {
-          logger.info({
-            JWT_ACCESS_TOKEN_VERIFY_ERROR: {
-              message: "Access token verification failed.",
-              error: err,
-            },
-          });
-
-          throw new ErrorHandler(STATUSCODE.UNAUTHORIZED, "Unauthorized");
-        }
-
-        return decoded;
-      }
-    );
+  verifyAccessToken(access_token: string): JWTPayload {
+    try {
+      return jwt.verify(
+        access_token,
+        process.env.JWT_ACCESS_SECRET_KEY as string
+      ) as JWTPayload;
+    } catch (err) {
+      logger.info({
+        VERIFY_ACCESS_TOKEN_ERROR: {
+          message: "Access Token Verification failed.",
+          err: err,
+        },
+      });
+      throw new ErrorHandler(STATUSCODE.UNAUTHORIZED, "Unauthorized");
+    }
   }
 
   generateRefreshToken(payload: any) {
