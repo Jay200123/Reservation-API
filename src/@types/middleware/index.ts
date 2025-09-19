c5a793264b0f3e9da5c0cbba54eaae89a56795c7
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-19 20:35:41 +0800

    Fxed Middleware custom type exports into single export object for cleaner and consistent type exporting.

--- src/@types/middleware/index.ts
+++ src/@types/middleware/index.ts
@@ -1,22 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 import { ErrorHandler } from "../../@utils";
+import { Users } from "../models/user";
 
-export type ErrorMiddlewareFn = (
+type ErrorMiddlewareFn = (
   err: ErrorHandler,
   req: Request,
   res: Response,
   next: NextFunction
 ) => void;
 
-export type SuccessMiddleware = (
+type SuccessMiddleware = (
   res: Response,
   statusCode: number,
   data: any,
   message: string
 ) => void;
 
-export type MiddlewareFn = (
+type MiddlewareFn = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => Promise<void> | void;
+
+type JWTPayload = {
+  user: Users;
+};
+export { ErrorMiddlewareFn, SuccessMiddleware, MiddlewareFn, JWTPayload };
