51b9e8da0836c4afd63bb7257819ab6849d21805
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-20 21:20:10 +0800

    Add `status` with type ReservationStatus Union for checking default reservation status values & `reservation_date`

--- src/@types/models/reservations.ts
+++ src/@types/models/reservations.ts
@@ -4,13 +4,16 @@ import { Service } from "./service";
 import { Timeslot } from "./timeslots";
 
 type PaymentType = "CASH" | "ONLINE_PAYMENT";
+type ReservationStatus = "PENDING" | "ONGOING" | "FINISHED";
 
 export type Reservations = {
   user: mongoose.Types.ObjectId | Users;
   services: mongoose.Types.ObjectId | Service;
   timeslot: mongoose.Types.ObjectId | Timeslot;
   payment_type: PaymentType;
+  status: ReservationStatus;
   amount: number;
+  reservation_date: Date;
   createdAt: Date;
   updatedAt: Date;
 };
