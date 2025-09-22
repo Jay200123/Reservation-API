05810ec7fb13aa05f6999c0f884473bf4306930f
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-22 22:19:49 +0800

    Add serviceRepository as new dependency in ReservationService class

--- src/@routes/reservation/route.ts
+++ src/@routes/reservation/route.ts
@@ -2,13 +2,16 @@ import express from "express";
 import Reservation from "./model";
 import Settings from "../settings/model";
 import UserCredentials from "../auth/model";
+import Service from "../service/model";
 import ReservationRepository from "./repository";
 import SettingsRepository from "../settings/repository";
 import AuthRepository from "../auth/repository";
+import ServiceRepository from "../service/repository";
 import ReservationService from "./service";
 import ReservationController from "./controller";
 import { AuthMiddleware } from "../../@middleware";
 import { JWT } from "../../@utils";
+import { createReservationFields } from "../../@validations";
 import { PATH, ROLE } from "../../@constants";
 
 const router = express.Router();
@@ -16,8 +19,12 @@ const router = express.Router();
 const reservationRepository = new ReservationRepository(Reservation);
 const settingsRepository = new SettingsRepository(Settings);
 const authRepository = new AuthRepository(UserCredentials);
+const serviceRepository = new ServiceRepository(Service);
 
-const reservationService = new ReservationService(reservationRepository);
+const reservationService = new ReservationService(
+  reservationRepository,
+  serviceRepository
+);
 
 const reservationController = new ReservationController(reservationService);
 
@@ -45,7 +52,8 @@ router.get(
 router.post(
   PATH.RESERVATIONS,
   authMiddleware.AccessTokenVerifier(),
-  authMiddleware.UserRoleVerifier(ROLE.USER),
+  authMiddleware.UserRoleVerifier(ROLE.USER, ROLE.ADMIN),
+  createReservationFields,
   reservationController.createReservation
 );
 
