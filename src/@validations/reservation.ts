a38310019c33e4e11908833ba8e0d2836e8a2cfb
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-23 20:05:22 +0800

    Fixed services field validation to accepts array instead of string

--- src/@validations/reservation.ts
+++ src/@validations/reservation.ts
@@ -2,7 +2,9 @@ import { body } from "express-validator";
 
 export const createReservationFields = [
   body("user").trim().escape().notEmpty().withMessage("user required"),
-  body("services").trim().escape().notEmpty().withMessage("services required"),
+  body("services")
+    .isArray({ min: 1 })
+    .withMessage("services must be type of arrays"),
   body("timeslot").trim().escape().notEmpty().withMessage("timeslot required"),
   body("payment_type")
     .trim()
