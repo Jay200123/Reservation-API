f4e0ff047c66b945972d0734aff45c214d5a6b63
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-09-24 09:41:41 +0800

    Add new value "RESCHEDULED" union type in `ReservationStatus` and new Reservation type property `reason`

--- src/@types/models/reservations.ts
+++ src/@types/models/reservations.ts
@@ -4,7 +4,7 @@ import { Service } from "./service";
 import { Timeslot } from "./timeslots";
 
 type PaymentType = "CASH" | "ONLINE_PAYMENT";
-type ReservationStatus = "PENDING" | "ONGOING" | "FINISHED";
+type ReservationStatus = "PENDING" | "RESCHEDULED" | "ONGOING" | "FINISHED";
 type ServiceReservations = {
   service: mongoose.Types.ObjectId | Service;
 };
@@ -16,6 +16,7 @@ export type Reservations = {
   payment_type: PaymentType;
   status: ReservationStatus;
   amount: number;
+  reason: string;
   reservation_date: Date;
   createdAt: Date;
   updatedAt: Date;
