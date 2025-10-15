59ca1c19924315068b4836078e4e9a8f9831e28d
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-10-15 23:08:34 +0800

    Add `Image` property to Service type.

--- src/@types/models/service.ts
+++ src/@types/models/service.ts
@@ -1,8 +1,11 @@
+import { Image } from "./image";
+
 export type Service = {
   service_name: string;
   service_price: number;
   description: string;
   duration: string;
+  image: Image[];
   createdAt: Date;
   updatedAt: Date;
 };
