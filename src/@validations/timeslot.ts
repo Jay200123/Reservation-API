dfa1b01f51dfb0e1d1562c363ac9a088f1e2200e
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-11-23 21:11:01 +0800

    Add timeslotParams for req.params validation

--- src/@validations/timeslot.ts
+++ src/@validations/timeslot.ts
@@ -1,4 +1,4 @@
-import { body } from "express-validator";
+import { body, query } from "express-validator";
 
 export const createTimeslotValidation = [
   body("start_time")
@@ -7,4 +7,18 @@ export const createTimeslotValidation = [
     .notEmpty()
     .withMessage("start_time required"),
   body("end_time").trim().escape().notEmpty().withMessage("end_time required"),
+];
+
+export const timeslotParams = [
+  query("skip")
+    .notEmpty()
+    .withMessage("Missing parameters")
+    .bail()
+    .isInt({ min: 0 })
+    .withMessage("Invalid request"),
+  query("limit")
+    .notEmpty()
+    .withMessage("Missing parameters")
+    .isInt({ min: 0 })
+    .withMessage("Invalid request"),
 ];
