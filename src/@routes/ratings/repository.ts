c6d06cbb75ac726d881fdf21e06e3cc68ca7149e
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-11-28 21:56:12 +0800

    Add RatingsRepository class with ratings data management async functions

---
+++ src/@routes/ratings/repository.ts
@@ -0,0 +1,30 @@
+import { Model } from "mongoose";
+import { Ratings } from "../../@types";
+
+export default class RatingsRepository {
+  constructor(private RatingsModel: Model<Ratings>) {}
+
+  async getAll(skip: number, limit: number) {
+    return await this.RatingsModel.find().skip(skip).limit(limit).lean().exec();
+  }
+
+  async getById(id: string) {
+    return await this.RatingsModel.findById(id).lean().exec();
+  }
+
+  async create(data: Ratings) {
+    return await this.RatingsModel.create({ ...data });
+  }
+
+  async updateById(id: string, data: Partial<Ratings>) {
+    return await this.RatingsModel.findByIdAndUpdate(
+      id,
+      { ...data, updatedAt: Date.now },
+      { new: true }
+    );
+  }
+
+  async deleteById(id: string) {
+    return await this.RatingsModel.findByIdAndDelete(id);
+  }
+}
