5594e054ab2310650b098d44f89cc607b00d9c02
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-11-28 21:56:41 +0800

    Add Ratings type

---
+++ src/@types/models/ratings.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { Users } from "./user";
+import { Service } from "./service";
+import { Image } from "./image";
+
+type Ratings = {
+  user: mongoose.Types.ObjectId | Users;
+  service: mongoose.Types.ObjectId | Service;
+  description: string;
+  rating: number;
+  images: Image[];
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type { Ratings };
