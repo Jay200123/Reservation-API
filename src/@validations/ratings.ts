34fbc9bafe953c9649e13bcfe866b1f95efbb629
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-12-01 22:34:05 +0800

    Add ratings field express-validator

---
+++ src/@validations/ratings.ts
@@ -0,0 +1,21 @@
+import { body } from "express-validator";
+
+const createRatingField = [
+  body("user").trim().escape().notEmpty().withMessage("user required"),
+  body("service").trim().escape().notEmpty().withMessage("service required"),
+  body("description")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("description required"),
+  body("rating")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("rating required")
+    .bail()
+    .isFloat({ min: 0 })
+    .withMessage("rating must be 0 or greater"),
+];
+
+export { createRatingField };
