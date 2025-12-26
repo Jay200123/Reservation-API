f907ffcd9eebb7434690df42bb345ead42fa4262
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-12-26 21:33:53 +0800

    Add new nginx localhost servers for containerized docker frontend react app

--- src/@config/whitelist.ts
+++ src/@config/whitelist.ts
@@ -1,3 +1,12 @@
-const whitelist = ["http://localhost:5173"];
+/**
+ * List of allowed origins for CORS.
+ * Used to validate incoming request origins.
+ */
+
+const whitelist = [
+  "http://localhost:5173", //for react-vite servers
+  "http://localhost:80", //nginx servers
+  "http://localhost",
+];
 
 export default whitelist;
