a45716f7cdcf037e9d1cbde26b838df4f903984b
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-12-26 21:35:28 +0800

    Add `GetUserRoleDashboard async  controller layer function

--- src/@routes/dashboard/controller.ts
+++ src/@routes/dashboard/controller.ts
@@ -1,5 +1,26 @@
+import { STATUSCODE } from "../../@constants";
+import { MiddlewareFn } from "../../@types";
+import { logger, SuccessHandler } from "../../@utils";
 import DashboardService from "./service";
 
 export default class DashboardController {
   constructor(private dashboardService: DashboardService) {}
+
+  GetUserRoleDashboard: MiddlewareFn = async (req, res, next) => {
+    logger.info({
+      GET_USER_ROLE_DASHBOARD_REQUEST: {
+        message: "SUCCESS",
+      },
+    });
+
+    const result = await this.dashboardService.GetUserRoleDashboard();
+
+    logger.info({
+      GET_USER_ROLE_DASHBOARD_RESPONSE: {
+        message: "SUCCESS",
+      },
+    });
+
+    return SuccessHandler(res, STATUSCODE.SUCCESS, result, "Success");
+  };
 }
