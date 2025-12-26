1f937824b37aedd9435a1ae87f46791144af9a4d
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2025-12-26 21:36:34 +0800

    Changed `DashboardService` Service Object layer

--- src/@routes/dashboard/service.ts
+++ src/@routes/dashboard/service.ts
@@ -1,15 +1,16 @@
-import UserRepository from "../users/repository";
-import ServiceRepository from "../service/repository";
-import RatingsRepository from "../ratings/repository";
-import ReservationRepository from "../reservation/repository";
-import TimeslotRepository from "../timeslot/repository";
-
+import { STATUSCODE } from "../../@constants";
+import { ErrorHandler } from "../../@utils";
+import DashboardRepository from "./repository";
 export default class DashboardService {
-  constructor(
-    private usersRepository: UserRepository,
-    private serviceRepository: ServiceRepository,
-    private ratingsRepository: RatingsRepository,
-    private reservationRepository: ReservationRepository,
-    private timeslotRepository: TimeslotRepository
-  ) {}
+  constructor(private dashboardRepository: DashboardRepository) {}
+
+  async GetUserRoleDashboard() {
+    const result = await this.dashboardRepository.UserRoleOverView();
+
+    if (!result) {
+      throw new ErrorHandler(STATUSCODE.BAD_REQUEST, "User data not found");
+    }
+
+    return result;
+  }
 }
